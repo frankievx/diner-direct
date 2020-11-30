49ecb3fac44f3251734d92a4355c1e2a006b5da1
Frankie <frankievx@gmail.com>
2020-11-30 13:11:31 -0500

    Implement Filters

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import useSWR from 'swr'
 import { useState, useEffect } from "react";
 import Table from '../components/table/Table'
 import SearchInput from '../components/SearchInput'
+import Sidebar from '../components/Sidebar'
 const fetcher = (query) =>
   fetch('/api/graphql', {
     method: 'POST',
@@ -21,6 +22,8 @@ export default function Index() {
   const [ offset, setOffset ] = useState(0)
   const [ limit, setLimit ] = useState(10)
   const [ search, setSearch ] = useState('')
+  const [term, setTerm] = useState("");
+
   const { data, mutate, error } = useSWR(
     `{ restaurants { items(offset:${offset}, limit:${limit}, search:"${search}") {name, city, state, phone, genre}, count(search:"${search}") } }`,
     fetcher
@@ -44,6 +47,12 @@ export default function Index() {
     { label: 'Phone', field: 'phone' },
     { label: 'Genre', field: 'genre', filter: true }
   ]
+
+  const sidebarConfig = {
+    searchHandler,
+    term,
+    setTerm
+  }
 
   const config = {
     filters,
@@ -78,13 +87,20 @@ export default function Index() {
   }
   
   return (
-    <div className="h-full container">
-      <div className="flex pt-8 justify-center">
-        <SearchInput onSearch={searchHandler} />
-      </div>
-      <div className="flex w-full mt-8">
-        <div className="lg:w-3/4  md:w-full m-auto my-auto">
-          <Table config={config} />
+    <div className="h-screen container">
+      <div className="flex">
+      <div className="w-72 h-screen bg-transparent">
+          <Sidebar config={sidebarConfig}/>
+        </div>
+        <div className="w-full mx-8">
+          {/* <div className="pt-8 mx-auto">
+            <SearchInput onSearch={searchHandler} />
+          </div> */}
+          <div className="mt-8">
+            <div className="md:w-full m-auto my-auto">
+              <Table config={config} />
+            </div>
+          </div>
         </div>
       </div>
     </div>
